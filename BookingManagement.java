9121a358c14604786f38495a940d922b49f1b770
G002heH40 <143741050+G002heH40@users.noreply.github.com>
2023-09-09 21:15:12 +0800

    Update BookingManagement.java

--- BookingManagement.java
+++ BookingManagement.java
@@ -1,61 +1,51 @@
+import java.io.*;
 import java.util.ArrayList;
-import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 
-public class BookingManagement 
-{
-    public static  ArrayList<Customer> customers;
-    public static  long durationInDays;
-    public static  String startBook;
-    public static  String endBook;
-    
-    public BookingManagement() {
-        customers = new ArrayList<>();
+public class BookingManagement {
+    private static String customerName;
+    private static String icNumber;
+    private static String contactInfo;
+    private static String licenseInfo;
+    private static String startDate;
+    private static String endDate;
+    private static int duration;
+    private static String rentCarNo;
+    private static double rentCarPay;
+
+    public BookingManagement(String customerName, String icNumber, String contactInfo, String licenseInfo) {
+        this.customerName = customerName;
+        this.icNumber = icNumber;
+        this.contactInfo = contactInfo;
+        this.licenseInfo = licenseInfo;
     }
 
-    public static void registerCustomer(String customerName, String customerIC, String customerContact, String customerLicense) {
-        Customer customer = new Customer(customerName, customerIC, customerContact, customerLicense);
-        customers.add(customer);
+    public String getStartDate() {
+        return startDate;
     }
 
-    public ArrayList<Customer> getAllCustomers() {
-        return customers;
+    public String getEndDate() {
+        return endDate;
     }
     
-    public String getStartBook()
-    {
-    	return startBook;
+    public static String getRentCarNo() {
+        return rentCarNo;
     }
-    
-    public String getEndBook()
-    {
-    	return endBook;
-    }
-    
-    
 
-    public static void registerCustomer() {
-        Scanner scanner = new Scanner(System.in);
-
+    public static double getRentCarPay() {
+        return rentCarPay;
+    }
+    
+    public static void registerCustomer(Scanner scanner) {
         System.out.println("Customer Registration");
-
-        // Prompt the user for customer details
         System.out.print("Enter customer name: ");
-        String customerName = scanner.nextLine();
-
+        customerName = scanner.nextLine();
         System.out.print("Enter customer IC: ");
-        String customerIC = scanner.nextLine();
-
+        icNumber = scanner.nextLine();
         System.out.print("Enter customer contact: ");
-        String customerContact = scanner.nextLine();
-
+        contactInfo = scanner.nextLine();
         System.out.print("Enter customer license: ");
-        String customerLicense = scanner.nextLine();
-
-        // Register the customer with the entered details
-        registerCustomer(customerName, customerIC, customerContact, customerLicense);
-
+        licenseInfo = scanner.nextLine();
         System.out.println("Customer registered successfully!");
     }
     
@@ -63,17 +53,17 @@ public class BookingManagement
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("Enter the date start booking (dd/mm/yyyy): ");
-        String startBook = scanner.nextLine();
+        startDate = scanner.nextLine();
 
         System.out.print("Enter the date end booking (dd/mm/yyyy): ");
-        String endBook = scanner.nextLine();
+        endDate = scanner.nextLine();
 
         // Define the date format pattern
         DateTimeFormatter inputFormatter = DateTimeFormatter.ofPattern("d/M/yyyy");
 
         // Check if the input date strings have single-digit days or months and add leading zeros if needed
-        String[] startParts = startBook.split("/");
-        String[] endParts = endBook.split("/");
+        String[] startParts = startDate.split("/");
+        String[] endParts = endDate.split("/");
         
         if (startParts[0].length() == 1) {
             startParts[0] = "0" + startParts[0];
@@ -89,12 +79,12 @@ public class BookingManagement
         }
 
         // Reconstruct the corrected date strings
-        startBook = String.join("/", startParts);
-        endBook = String.join("/", endParts);
+        startDate = String.join("/", startParts);
+        endDate = String.join("/", endParts);
 
         // Parse the input into LocalDate objects using the specified format
-        LocalDate date1 = LocalDate.parse(startBook, inputFormatter);
-        LocalDate date2 = LocalDate.parse(endBook, inputFormatter);
+        LocalDate date1 = LocalDate.parse(startDate, inputFormatter);
+        LocalDate date2 = LocalDate.parse(endDate, inputFormatter);
 
         // Calculate the duration between the two dates in days
         long durationInDays = Math.abs(date1.toEpochDay() - date2.toEpochDay());
@@ -105,8 +95,7 @@ public class BookingManagement
         // Display the result with the new format
         System.out.println("Duration between the two dates: " + outputFormatter.format(date1) +
                 " and " + outputFormatter.format(date2) + " is " + durationInDays + " days");
-
-        scanner.close();
+        
     }
 
     public static void checkAvailability(Scanner input, ArrayList<CarManager> cars) {
@@ -136,9 +125,58 @@ public class BookingManagement
         for (CarManager car : availableCars) {
             System.out.printf("%-15s || %-20s || %-15.2f%n", car.getPlateno(), car.getModel(), car.getRate());
         }
-    }    
-
-    public static void CancelBooking() {
-        System.out.println("Booking has been canceled.");
     }
+
+    public static void selectCar(Scanner input, ArrayList<CarManager> cars) {
+        System.out.print("Enter the plate number of the car you want to select: ");
+        String plateNumber = input.nextLine();
+
+        // Find the car with the specified plate number
+        for (CarManager car : cars) {
+            if (car.getPlateno().equalsIgnoreCase(plateNumber) && car.getStatus().equalsIgnoreCase("Available")) 
+            {
+                rentCarNo = car.getPlateno();
+                rentCarPay = car.getRate();
+                car.setStatus("Not Available"); // Update car status to Not Available
+                return; // Return the selected car
+            }
+        }
+
+        System.out.println("Car not found with the specified plate number or not available.");
+        return; // Return null if the car is not found or not available
+    }
+    
+    // Method to store booking details and save to a file
+    public static void storeBookingDetails() {
+        // Implement code to store booking details into an array or object.
+        // Include customer details, chosen car details, booking dates, duration, and payment rate.
+
+        // Create a string with booking details
+        String bookingDetail = customerName + "," + icNumber + "," + contactInfo + ","
+                + licenseInfo + "," + BookingManagement.getRentCarNo()+ "," + startDate + ","
+                + endDate + "," + duration + "," + BookingManagement.getRentCarPay();
+
+        // Save the booking detail to a file
+        saveBookingDetailToFile(bookingDetail);
+    }
+
+    private static void saveBookingDetailToFile(String bookingDetail) {
+        try {
+            FileWriter writer = new FileWriter("bookingDetail.txt", true); // Append mode
+            writer.write(bookingDetail + "\n");
+            writer.close();
+            System.out.println("Booking details saved successfully.");
+        } catch (IOException e) {
+            System.out.println("Error saving booking details: " + e.getMessage());
+        }
+    }
+
+    
+	public static void CancelBooking() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
+
