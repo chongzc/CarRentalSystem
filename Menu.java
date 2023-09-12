51739b25e43d3e10e7aa777429ca116b6d351219
G002heH40 <143741050+G002heH40@users.noreply.github.com>
2023-09-12 23:56:54 +0800

    Update Menu.java

--- Menu.java
+++ Menu.java
@@ -5,14 +5,14 @@ import java.util.*;
 
 public class Menu {
 	
-	public static void Login(){
-		 boolean isVerified = false;
+	public static void Login(){//for admin to login into the system
+		boolean isVerified = false;
 	        Scanner scanner = new Scanner(System.in);
 
 	        while (!isVerified) {
 		    System.out.println("-----------------------------------------");
 	            System.out.println("|     Welcome to Car Rental System      |");
-		    System.out.println("-----------------------------------------");
+		    System.out.println("-----------------------------------------"); //display banner
 	            System.out.print("Enter username: ");
 	            String enteredUsername = scanner.nextLine();
 	            System.out.print("Enter password: ");
@@ -20,14 +20,14 @@ public class Menu {
 
 	            if (enteredUsername.equals(User.getUsername()) && enteredPassword.equals(User.getPassword())) {
 	                isVerified = true;
-	                System.out.println("Login successfully!\n");
+	                System.out.println("Login successfully!\n");//if username and password correct, login successful ,exit while loop
 	            } else {
-	                System.out.println("Wrong username or password! Try again.\n");
+	                System.out.println("Wrong username or password! Try again.\n");//login fail, back into while loop
 	            }
 	        }
 	}
 	
-	public static void MainMenu() {
+	public static void MainMenu() {//to show the main menu
         System.out.println( "\nMain Menu");
         System.out.println( "1 - Car Menu");
         System.out.println( "2 - Booking");
@@ -37,7 +37,7 @@ public class Menu {
         System.out.print  ( "Your choice: ");
     }
 	
-	public static void displayCarMenu() {
+	public static void displayCarMenu() {//to show the Car Menu
         System.out.println( "\nCar Menu");
         System.out.println( "1 - Add a car");
         System.out.println( "2 - Display cars");
@@ -48,7 +48,7 @@ public class Menu {
         System.out.print  ( "Your choice: ");
     }
 			
-	public static void displayBookingMenu() {
+	public static void displayBookingMenu() {//to show the Booking Menu
         System.out.println( "\nBooking Menu");
         System.out.println( "1 - Check Availability");
         System.out.println( "2 - Rent a car");
@@ -58,14 +58,14 @@ public class Menu {
         System.out.print  ( "Your choice: ");
     }
 	
-	public static void displayHistoryMenu() {
+	public static void displayHistoryMenu() {//to show the History Menu
         System.out.println( "\nHistory Menu");
         System.out.println( "1 - View Booking History");
         System.out.println( "2 - View Payment History");
         System.out.println( "3 - Quit");
         System.out.print  ( "Your choice: ");
     }
-	public static void displayPickupReturnMenu() {
+	public static void displayPickupReturnMenu() {//to show the Return Menu
         System.out.println( "\nPickup and Return");
         System.out.println( "1 - Pickup");
         System.out.println( "2 - Return");
