be21975a48ef808bcecbd8572da946f21da5690c
G002heH40 <143741050+G002heH40@users.noreply.github.com>
2023-09-12 23:51:33 +0800

    Update FileManagement.java

--- FileManagement.java
+++ FileManagement.java
@@ -7,19 +7,22 @@ public class FileManagement {
     
     private File carFile;
     
-   
+    //constructor
     public FileManagement(String carFile) {
         this.carFile = new File(carFile);
     }
-    
+
+    //getter
     public ArrayList<CarManager> getListOfCars() {
         return carsList;
     }
 
+    //setter
     public void setListOfCars(ArrayList<CarManager> carsList) {
         this.carsList = carsList;
     }
 
+    //add car into carsList
     public void addCar(String model, int seat, String plateno, String power, String engine, String category, double rate, String status) {
         carsList.add(new CarManager(model, seat, plateno, power, engine, category, rate, status));
     }
@@ -31,7 +34,7 @@ public class FileManagement {
         try (FileWriter carWriter = new FileWriter(carFile, false)) {
             for (CarManager car : carsList) {
                 String line = String.format("%s,%d,%s,%s,%s,%s,%.2f,%s\n", car.getModel(), car.getSeats(),
-                        car.getPlateno(), car.getPower(), car.getEngine(), car.getCategory(), car.getRate(), car.getStatus());
+                        car.getPlateno(), car.getPower(), car.getEngine(), car.getCategory(), car.getRate(), car.getStatus());//the format to write in txt file
 
                 carWriter.write(line);
             }
@@ -59,9 +62,9 @@ public class FileManagement {
                     String engine = carData[4].trim();
                     String category = carData[5].trim();
                     double rate = Double.parseDouble(carData[6].trim());
-                    String status = carData[7].trim(); // Read status as a string
+                    String status = carData[7].trim(); // Read the data in the txt file, assign variable for every elements in 1 line
 
-                    carsList.add(new CarManager(model, seats, plateno, power, engine, category, rate, status));
+                    carsList.add(new CarManager(model, seats, plateno, power, engine, category, rate, status));//add into carList
                 }
             }
         } finally {
